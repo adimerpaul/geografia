0aef97f947ddde373349115fd85b7af92c366ce0
adimerpaul <adimer101@gmail.com>
2024-05-16 00:15:54 -0400

    comenzado el api de lugares

---
+++ includes/header.php
@@ -0,0 +1,123 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("location: index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Geografica</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+         folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Google Font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+
+    <header class="main-header">
+        <!-- Logo -->
+        <a href="welcome.php" class="logo">
+            <!-- mini logo for sidebar mini 50x50 pixels -->
+            <span class="logo-mini"><b>G</b>EO</span>
+            <!-- logo for regular state and mobile devices -->
+            <span class="logo-lg"><b>Geo</b>grafica</span>
+        </a>
+        <!-- Header Navbar: style can be found in header.less -->
+        <nav class="navbar navbar-static-top">
+            <!-- Sidebar toggle button-->
+            <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+
+            <div class="navbar-custom-menu">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="salir.php"><i class="fa fa-sign-out"></i> Salir</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <!-- =============================================== -->
+
+    <!-- Left side column. contains the sidebar -->
+    <aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+            <!-- Sidebar user panel -->
+            <div class="user-panel">
+                <div class="pull-left image">
+                    <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                </div>
+                <div class="pull-left info">
+                    <p><?php echo $_SESSION['username']; ?></p>
+                    <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+                </div>
+            </div>
+            <!-- sidebar menu: : style can be found in sidebar.less -->
+            <ul class="sidebar-menu" data-widget="tree">
+                <li class="header">MENU</li>
+                <li>
+                    <a href="welcome.php">
+                        <i class="fa fa-map"></i> <span>Lugares</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="brigadas.php">
+                        <i class="fa fa-users"></i> <span>Brigadas</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="agenda.php">
+                        <i class="fa fa-calendar"></i> <span>Agenda</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="rutas.php">
+                        <i class="fa fa-map-marker"></i> <span>Rutas</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="cerrar.php">
+                        <i class="fa fa-sign-out"></i> <span>Cerrar Sesión</span>
+                    </a>
+                </li>
+            </ul>
+        </section>
+        <!-- /.sidebar -->
+    </aside>
+
+    <!-- =============================================== -->
+
+    <!-- Content Wrapper. Contains page content -->
--- welcome.php
+++ welcome.php
@@ -1,198 +1,126 @@
-<?php
-session_start();
-if (!isset($_SESSION['username'])) {
-    header("location: index.php");
-}
-?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Geografica</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-    <!-- AdminLTE Skins. Choose a skin from the css/skins
-         folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    <!-- Google Font -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-</head>
-<body class="hold-transition skin-blue sidebar-mini">
-<!-- Site wrapper -->
-<div class="wrapper">
-
-    <header class="main-header">
-        <!-- Logo -->
-        <a href="welcome.php" class="logo">
-            <!-- mini logo for sidebar mini 50x50 pixels -->
-            <span class="logo-mini"><b>G</b>EO</span>
-            <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><b>Geo</b>grafica</span>
-        </a>
-        <!-- Header Navbar: style can be found in header.less -->
-        <nav class="navbar navbar-static-top">
-            <!-- Sidebar toggle button-->
-            <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </a>
-
-            <div class="navbar-custom-menu">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="salir.php"><i class="fa fa-sign-out"></i> Salir</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
-
-    <!-- =============================================== -->
-
-    <!-- Left side column. contains the sidebar -->
-    <aside class="main-sidebar">
-        <!-- sidebar: style can be found in sidebar.less -->
-        <section class="sidebar">
-            <!-- Sidebar user panel -->
-            <div class="user-panel">
-                <div class="pull-left image">
-                    <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-                </div>
-                <div class="pull-left info">
-                    <p><?php echo $_SESSION['username']; ?></p>
-                    <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-                </div>
-            </div>
-            <!-- sidebar menu: : style can be found in sidebar.less -->
-            <ul class="sidebar-menu" data-widget="tree">
-                <li class="header">MENU</li>
-                <li>
-                    <a href="lugares.php">
-                        <i class="fa fa-map"></i> <span>Lugares</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="brigadas.php">
-                        <i class="fa fa-users"></i> <span>Brigadas</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="agenda.php">
-                        <i class="fa fa-calendar"></i> <span>Agenda</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="rutas.php">
-                        <i class="fa fa-map-marker"></i> <span>Rutas</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="cerrar.php">
-                        <i class="fa fa-sign-out"></i> <span>Cerrar Sesión</span>
-                    </a>
-                </li>
-            </ul>
-        </section>
-        <!-- /.sidebar -->
-    </aside>
-
-    <!-- =============================================== -->
-
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
+<?php include 'includes/header.php'; ?>
+<div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <section class="content-header">
             <h1>
-                Blank page
-                <small>it all starts here</small>
+                Lugares
+                <small>Panel de control</small>
             </h1>
             <ol class="breadcrumb">
                 <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-                <li><a href="#">Examples</a></li>
-                <li class="active">Blank page</li>
+                <li><a href="#">Lugares</a></li>
             </ol>
         </section>
-
-        <!-- Main content -->
         <section class="content">
-
-            <!-- Default box -->
             <div class="box">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Title</h3>
+                <div class="box-body no-padding">
+                    <div class="text-right" style="padding: 10px;">
+                        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#modal-default">
+                            <i class="fa fa-plus"></i> Agregar
+                        </button>
+                    </div>
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th style="width: 10px">Opciones</th>
+                                <th style="width: 10px">#</th>
+                                <th>Descripcion</th>
+                                <th>Prioridad</th>
+                                <th>Tipo</th>
+                                <th style="width: 40px">Ubicación</th>
+                            </tr>
+                        </thead>
+                        <tr>
+                            <td>
+                                <a href="editar_lugar.php" class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></a>
+                                <a href="eliminar_lugar.php" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i></a>
+                            </td>
+                            <td>1.</td>
+                            <td>Desripcion</td>
+                            <td>Destion</td>
+                            <td>CINFA</td>
+                            <td>
+                                <a href="https://www.google.com/maps/place/14.634915,-90.506882" target="_blank">
+                                    <i class="fa fa-map"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    </table>
+                    <div class="modal fade" id="modal-default">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span></button>
+                                    <h4 class="modal-title">Nuevo Lugar</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <form class="form-horizontal">
+                                        <div class="box-body">
+                                            <div class="form-group">
+                                                <label for="descripcion" class="col-sm-2 control-label">Descripcion</label>
 
-                    <div class="box-tools pull-right">
-                        <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                                title="Collapse">
-                            <i class="fa fa-minus"></i></button>
-                        <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-                            <i class="fa fa-times"></i></button>
+                                                <div class="col-sm-4">
+                                                    <input type="text" class="form-control" id="descripcion" placeholder="Descripcion">
+                                                </div>
+                                                <label for="prioridad" class="col-sm-2 control-label">Prioridad</label>
+                                                <div class="col-sm-4">
+                                                    <select type="text" class="form-control" id="prioridad">
+                                                        <option value="0">Destino</option>
+                                                        <option value="1">Origen</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="tipo" class="col-sm-2 control-label">Tipo</label>
+                                                <div class="col-sm-4">
+                                                    <select type="text" class="form-control" id="tipo">
+                                                        <option value="CINFA">CINFA</option>
+                                                        <option value="HOSPITAL">HOSPITAL</option>
+                                                        <option value="ALMACEN">ALMACEN</option>
+                                                        <option value="EXTERNO">EXTERNO</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div id="map" style="width: 100%; height: 400px;"></div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-danger pull-left" data-dismiss="modal">Cerrar</button>
+                                            <button type="button" class="btn btn-primary" id="guardar">Guardar</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <!-- /.modal-content -->
+                        </div>
+                        <!-- /.modal-dialog -->
                     </div>
                 </div>
-                <div class="box-body">
-                    Start creating your amazing application!
-                </div>
-                <!-- /.box-body -->
-                <div class="box-footer">
-                    Footer
-                </div>
-                <!-- /.box-footer-->
             </div>
-            <!-- /.box -->
-
         </section>
-        <!-- /.content -->
     </div>
-    <!-- /.content-wrapper -->
-
-    <footer class="main-footer">
-        <div class="pull-right hidden-xs">
-            <b>Version</b> 2.4.0
-        </div>
-        <strong>Copyright &copy; <?php echo date('Y')?>.</strong> Todos los derechos reservados.
-    </footer>
-
-    <!-- Add the sidebar's background. This div must be placed
-         immediately after the control sidebar -->
-    <div class="control-sidebar-bg"></div>
-</div>
-<!-- ./wrapper -->
-
-<!-- jQuery 3 -->
-<script src="bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- SlimScroll -->
-<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="bower_components/fastclick/lib/fastclick.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
+<?php include 'includes/footer.php'; ?>
 <script>
-    $(document).ready(function () {
-        $('.sidebar-menu').tree()
-    })
+    window.onload = function () {
+        $('#modal-default').on('shown.bs.modal', function () {
+            map.invalidateSize();
+        });
+        var orurolat = -17.964888;
+        var orurolng = -67.115827;
+        var map = L.map('map').setView([orurolat, orurolng], 13);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19
+        }).addTo(map);
+        var marker = L.marker([orurolat, orurolng], {
+            draggable: true
+        }).addTo(map);
+        map.on('click', function (e) {
+            orurolat = e.latlng.lat;
+            orurolng = e.latlng.lng;
+            marker.setLatLng(e.latlng);
+        });
+        $('#guardar').click(function () {
+            console.log(orurolat, orurolng);
+        });
+    }
 </script>
-</body>
-</html>
